98927a5f0d6d16e91bd801651ac0cfb6a0dce4c3
yangjinee <yangjinee@naver.com>
2023-03-20 13:58:51 +0900

    ⛄[BE : feat] metadata save API 구현
    JIRA Code : S08P21S002-189

--- API_validator_server/controllers/metadataController.js
+++ API_validator_server/controllers/metadataController.js
@@ -1,17 +1,55 @@
 const metadataService = require("../services/metadataService");
 
+const getAllMetadatas = async (req, res) => {
+  const {apiId} = req.params;
+
+  try {
+    const data = metadataService.getAllMetadatas(apiId);
+
+    //await apiService.deleteOneApi(apiId);
+    res.status(204).send({ status: "OK" });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: "FAILED", data: { error: error?.message || error } });
+  }
+};
 
 const createMetadata = async (req, res) => {
+    const {apiId} = req.params;
+    const {body} = req;
     
     try {
-      //await apiService.deleteOneApi(apiId);
-      res.status(204).send({ status: "OK" });
+      const api_id = await metadataService.createMetadata(apiId, body);
+      res.status(200).send({ "api_id" : api_id });
     } catch (error) {
       res
         .status(error?.status || 500)
         .send({ status: "FAILED", data: { error: error?.message || error } });
     }
 };
+
+const updateMetadata = async (req, res) => {
+  const {metaId} = req.params;
+  try{
+      const data = await metadataService.updateMetadata(metaId);
+      res.send({ data });
+  }catch (error) { 
+
+  }
+};
+
+const deleteMetadata = async (req, res) => {
+  const {metaId} = req.params;
+  try{
+      const data = await metadataService.updateMetadata(metaId);
+      res.send({ data });
+  }catch (error) { 
+
+  }
+};
+
+
 const testMetadata = async (req, res) => {
     const {metaId} = req.params;
     try{
@@ -22,9 +60,23 @@ const testMetadata = async (req, res) => {
     }
 };
 
+
+const createExpectResponse = async (req, res) => {
+  const {metaId} = req.params;
+  try{
+      const data = await metadataService.updateMetadata(metaId);
+      res.send({ data });
+  }catch (error) { 
+
+  }
+};
   
   module.exports = {
+    getAllMetadatas,
     createMetadata,
+    updateMetadata,
+    deleteMetadata,
     testMetadata,
+    createExpectResponse
   };
   
