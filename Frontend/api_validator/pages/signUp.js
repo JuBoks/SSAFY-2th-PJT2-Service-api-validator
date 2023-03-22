76a0ac6c0f5b8e410a5574e8a27c92a6b7adc507
Jeong UiGwon <jek9412@naver.com>
2023-03-22 15:53:39 +0900

    🥑[FE: Feat] Create Admin API UI
    관리자 API 관리 UI 구현
    
    JIRA Code: S08P21S002-238

--- Frontend/api_validator/pages/signUp.js
+++ Frontend/api_validator/pages/signUp.js
@@ -22,7 +22,6 @@ const SignUp = () => {
 
   const handleTypeChange = (props) => setType(props.target.value);
 
-  // email 변경시 이벤트 함수
   const handleEmailChange = async (props) => {
     try {
       if (!props.target.value) return;
