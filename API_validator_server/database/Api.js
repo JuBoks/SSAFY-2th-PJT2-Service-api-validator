96c0678fbc80beeb1805b33d442408ef76442abf
Seonghwan Kim <seonghwan1031@gmail.com>
2023-03-22 12:16:48 +0900

    🍺[BE : fix] Refactor API CRUD

--- API_validator_server/database/Api.js
+++ API_validator_server/database/Api.js
@@ -1,12 +1,16 @@
+const http_method = { 0: "GET", 1: "POST", 2: "PUT", 3: "DELETE" };
+
 const createNewApi = async (conn, newApi) => {
   try {
-    let sql = `SELECT * From tbl_api WHERE method = ? resources = ? and domain_id = ? and state = 0`;
+    let sql = `SELECT * From tbl_api WHERE method = ? and resources = ? and domain_id = ? and state = 0`;
     let params = [newApi.method, newApi.resources, newApi.domain_id];
     let [rows, _] = await conn.query(sql, params);
     if (rows.length) {
       throw {
         status: 400,
-        message: `Api '${newApi.method}':'${newApi.resources}' already exists`,
+        message: `Api '${http_method[newApi.method]}':'${
+          newApi.resources
+        }' already exists`,
       };
     }
     sql = "INSERT INTO tbl_api (method, resources, domain_id) values (?, ?, ?)";
@@ -31,7 +35,7 @@ const getAllApis = async (conn, domainId) => {
 
 const getOneApi = async (conn, apiId) => {
   try {
-    let sql = "SELECT * FROM tbl_api WHERE api_id = ? WHERE state = 0";
+    let sql = "SELECT * FROM tbl_api WHERE api_id = ? and state = 0";
     let params = [apiId];
     let [rows, _] = await conn.query(sql, params);
     if (!rows.length) {
@@ -48,24 +52,27 @@ const getOneApi = async (conn, apiId) => {
 
 const updateOneApi = async (conn, apiId, changes) => {
   try {
-    let sql = `SELECT * From tbl_api WHERE method = ? resources = ? and domain_id = ? and state = 0`;
-    let params = [newApi.method, newApi.resources, newApi.domain_id];
+    let sql = `SELECT * From tbl_api WHERE api_id = ? and state = 0`;
+    let params = [apiId];
     let [rows, _] = await conn.query(sql, params);
-    if (rows.length) {
-      throw {
-        status: 400,
-        message: `Api '${newApi.method}':'${newApi.resources}' already exists`,
-      };
-    }
-    sql = `SELECT * From tbl_api WHERE api_id = ? and state = 0`;
-    params = [apiId];
-    [rows, _] = await conn.query(sql, params);
     if (!rows.length) {
       throw {
         status: 400,
         message: `Can't find api with the id '${apiId}'`,
       };
     }
+    sql = `SELECT * From tbl_api WHERE method = ? and resources = ? and domain_id = ? and state = 0`;
+    params = [changes.method, changes.resources, changes.domain_id];
+    [rows, _] = await conn.query(sql, params);
+    if (rows.length) {
+      throw {
+        status: 400,
+        message: `Api '${http_method[changes.method]}':'${
+          changes.resources
+        }' already exists`,
+      };
+    }
+
     sql =
       "UPDATE tbl_api SET method = ?, resources = ?, domain_id = ? WHERE api_id = ?";
     params = [changes.method, changes.resources, changes.domain_id, apiId];
