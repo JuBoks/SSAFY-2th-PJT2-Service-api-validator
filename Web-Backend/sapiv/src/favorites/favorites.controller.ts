0862d65d1e806f7b7e8f7439f3dd444ad897e324
SangWoo <brianpsw@naver.com>
2023-03-23 23:47:22 +0900

    🍤[BE: feat] Add Create Favorite feature Users can add new apis to their favorite Jira: None

--- Web-Backend/sapiv/src/favorites/favorites.controller.ts
+++ Web-Backend/sapiv/src/favorites/favorites.controller.ts
@@ -1,8 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req } from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
 import { CreateFavoriteDto } from './dto/create-favorite.dto';
 import { UpdateFavoriteDto } from './dto/update-favorite.dto';
 import { AllowUnauthorizedRequest } from 'src/common/guard/allow-unauthorized-request';
+import { CustomRequest } from 'src/common/custromrequest';
+import { AppAbility } from 'src/casl/casl-ability.factory/casl-ability.factory';
+import { CheckPolicies } from 'src/common/guard/policies-guard';
+import { User } from 'src/users/entities/user.entity';
+import { Action } from 'src/casl/action';
 import { Favorite } from './entities/favorite.entity';
 
 @Controller('favorites')
@@ -10,15 +15,15 @@ export class FavoritesController {
   constructor(private readonly favoritesService: FavoritesService) {}
 
   @Post()
-  @AllowUnauthorizedRequest()
-  create(@Body() createFavoriteDto: Favorite) {
-    return this.favoritesService.create(createFavoriteDto);
+  @CheckPolicies((ability: AppAbility) => ability.can(Action.Create, Favorite))
+  async create(@Body() createFavoriteDto: CreateFavoriteDto, @Req() req:CustomRequest) {
+    return await this.favoritesService.create(createFavoriteDto, req);
   }
 
   @Get()
-  @AllowUnauthorizedRequest()
-  findAll() {
-    return this.favoritesService.findAll();
+  @CheckPolicies((ability: AppAbility) => ability.can(Action.Read, Favorite))
+  async findAll(@Req() req:CustomRequest) {
+    return await this.favoritesService.findAll(req);
   }
 
   @Get(':id')
