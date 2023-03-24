6431122bac60d18a9324c5e79f2cea1a8f866f42
yangjinee <yangjinee@naver.com>
2023-03-24 14:38:45 +0900

    ⛄[BE : feat] 특정 API 의 pass/fail 주기별로 읽어오기 구현
    month, week, day 단위로 요청을 보내 읽을 수 있음
    
    JIRA Code : S08P21S002-244

--- API_validator_server/app.js
+++ API_validator_server/app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const validatorRouter = require("./routes/validatorRoutes");
-const apiRouter = require("./routes/apiRoutes");
+const sapivRouter = require("./routes/sapivRoutes");
+const { authCheck } = require("./middleware/authCheck");
+
 
 const app = express();
 const PORT = 3000; //서버 포트 번호
@@ -11,7 +13,8 @@ app.disable("x-powered-by");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/validator", validatorRouter);
-app.use("/api", apiRouter);
+
+app.use("/api",authCheck, sapivRouter);
 
 
 app.listen(PORT, () => {
