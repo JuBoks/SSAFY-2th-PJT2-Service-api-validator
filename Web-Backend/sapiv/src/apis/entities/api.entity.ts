54a66deb4e52a5ec6d72bc7587eeb28e40c707fd
Park Sang Woo <brianpsw@naver.com>
2023-03-24 18:01:41 +0900

    🍤[FE: feat] Add Favorite add and read function Connect DB with typeORM JIRA Code : None

--- Web-Backend/sapiv/src/apis/entities/api.entity.ts
+++ Web-Backend/sapiv/src/apis/entities/api.entity.ts
@@ -1,1 +1,13 @@
-export class Api {}
+export class Api {
+    "meta_id": number;
+    "category_name": string;
+    "domain": string; 
+    "resouces": string;
+    "method": number;
+    "header": object;
+    "params": object;
+    "body": object;
+    "name": string; 
+    "cycle_time": number;
+    "last_req_time": number;
+}
--- Web-Backend/sapiv/src/favorites/favorites.controller.ts
+++ Web-Backend/sapiv/src/favorites/favorites.controller.ts
@@ -6,11 +6,15 @@ import { AllowUnauthorizedRequest } from 'src/common/guard/allow-unauthorized-re
 import { CustomRequest } from 'src/common/custromrequest';
 import { AppAbility } from 'src/casl/casl-ability.factory/casl-ability.factory';
 import { CheckPolicies } from 'src/common/guard/policies-guard';
-import { User } from 'src/users/entities/user.entity';
 import { Action } from 'src/casl/action';
 import { Favorite } from './entities/favorite.entity';
+import { ApiHeader } from '@nestjs/swagger';
 
 @Controller('favorites')
+@ApiHeader({
+  name: 'idtoken',
+  description: 'Custom header',
+})
 export class FavoritesController {
   constructor(private readonly favoritesService: FavoritesService) {}
 
@@ -26,18 +30,8 @@ export class FavoritesController {
     return await this.favoritesService.findAll(req);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.favoritesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFavoriteDto: UpdateFavoriteDto) {
-    return this.favoritesService.update(+id, updateFavoriteDto);
-  }
-
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.favoritesService.remove(+id);
+  remove(@Param('id') id: string, @Req() request: CustomRequest) {
+    return this.favoritesService.remove(+id, request);
   }
 }
