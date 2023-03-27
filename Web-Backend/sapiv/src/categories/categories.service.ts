d2a2a1a1c8bd43b4b151c0f9234aff7aca231cc2
Park Sang Woo <brianpsw@naver.com>
2023-03-27 12:16:35 +0900

    🍤[BE: feat] Add Axios Call to Validator Server Add Axios Call to Validator Server JIRA Code :None

--- Web-Backend/sapiv/src/categories/categories.service.ts
+++ Web-Backend/sapiv/src/categories/categories.service.ts
@@ -1,15 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { HttpException, Inject, Injectable, Logger } from '@nestjs/common';
+import { AxiosError } from 'axios';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { firstValueFrom, catchError } from 'rxjs';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Category } from './entities/category.entity';
 
 @Injectable()
 export class CategoriesService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(private readonly httpService: HttpService, @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger) {}
+  async create(createCategoryDto: CreateCategoryDto, request) {
+    const { data } = await firstValueFrom(
+      this.httpService.post<Category[]>('http://localhost:8070/validator').pipe(
+        catchError((error: AxiosError) => {
+          this.logger.error(error.response.data);
+          throw new HttpException(
+            error.message,
+            error.status
+          );
+        }),
+      ),
+    );
+    return data;
   }
 
-  findAll() {
-    return `This action returns all categories`;
+  async findAll() {
+    const { data } = await firstValueFrom(
+      this.httpService.get<Category[]>('http://api-validator/validator/api').pipe(
+        catchError((error: AxiosError) => {
+          this.logger.error(error.response.data);
+          throw new HttpException(
+            error.message,
+            error.status
+          );
+        }),
+      ),
+    );
+    return data;
   }
 
   findOne(id: number) {
