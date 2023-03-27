99a95924b19ef600afee5762fa1b7140934d5e2d
Jeong UiGwon <jek9412@naver.com>
2023-03-27 19:37:10 +0900

    🥑[FE: feat] API 추가/수정 기능 구현 중
    API 추가/수정 기능 구현 중

--- Frontend/api_validator/pages/admin/apiEdit.js
+++ Frontend/api_validator/pages/admin/apiEdit.js
@@ -1,7 +1,13 @@
 import React from "react";
+import styles from "@/styles/Admin.module.css";
 import Header from "@/components/Header";
 import Nav from "@/components/Nav";
-import { Box, Toolbar, Typography } from "@mui/material";
+import { Box, Divider, Toolbar, Typography, TextField } from "@mui/material";
+import FreeSoloCreateOptionDialog from "@/components/FreeSoloCreateOptionDialog";
+import ComboBox from "@/components/ComboBox";
+import { methodList } from "@/constants/methodList";
+import { cycleList } from "@/constants/cycleList";
+import TabAPIinfo from "@/components/TabAPIinfo";
 
 export default function APIedit() {
   return (
@@ -11,7 +17,29 @@ export default function APIedit() {
         <Nav />
         <Box>
           <Toolbar />
-          <Typography variant="h3">Hello World</Typography>
+          <Box className={styles.main}>
+            <Typography className={styles.text} variant="h3">
+              API 편집
+            </Typography>
+            <Typography
+              className={styles.text}
+              mt={2}
+              mb={2}
+              variant="subtitle1"
+            >
+              API를 추가하거나 편집이 가능합니다.
+            </Typography>
+            <Divider />
+            <Box mt={3} display="flex">
+              <TextField id="name" label="Name" variant="outlined" />
+              <FreeSoloCreateOptionDialog label="Category" />
+              <FreeSoloCreateOptionDialog label="Service" />
+              <TextField id="path" label="Path" variant="outlined" />
+              <ComboBox label="Method" options={methodList} />
+              <ComboBox label="Cycle" options={cycleList} />
+            </Box>
+            <TabAPIinfo />
+          </Box>
         </Box>
       </Box>
     </>
