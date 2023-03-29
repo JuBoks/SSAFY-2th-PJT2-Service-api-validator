d482dd876268a29a367a6a00fce57dd023a1ded3
Park Sang Woo <brianpsw@naver.com>
2023-03-30 08:42:45 +0900

    🍤[BE: feat] Add Function to call all the testcase list Add Function to call all the testcase JIRA Code : None

--- Web-Backend/sapiv/src/apis/apis.module.ts
+++ Web-Backend/sapiv/src/apis/apis.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { ApisService } from './apis.service';
 import { ApisController } from './apis.controller';
 import { HttpModule } from '@nestjs/axios';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Domain } from 'domain';
+import { Category } from 'src/categories/entities/category.entity';
+import { Metadata } from 'src/metadatas/entities/metadata.entity';
+import { Api } from './entities/api.entity';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, TypeOrmModule.forFeature([Metadata, Api, Domain, Category])],
   controllers: [ApisController],
   providers: [ApisService]
 })
