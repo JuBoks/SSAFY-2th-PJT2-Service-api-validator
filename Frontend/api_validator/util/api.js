f18450cb662951ca796732a65177cf8bffc97f22
Jeong UiGwon <jek9412@naver.com>
2023-03-31 17:50:13 +0900

    🥑[FE: feat] 환경변수 설정
    웹 백엔드 주소와 Firebase 설정값 환경 변수 설정

--- Frontend/api_validator/util/api.js
+++ Frontend/api_validator/util/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://j8s002.p.ssafy.io/api",
+  baseURL: process.env.NEXT_PUBLIC_WEB_BECKEND_ADDRESS,
 });
 
 export const GetUsers = async (idToken) => {
--- Frontend/api_validator/util/firebaseApp.js
+++ Frontend/api_validator/util/firebaseApp.js
@@ -6,13 +6,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA2odxhCohwWkp2o-VMvTa-bCvGJj9Q7A0",
-  authDomain: "sapiv-3fb24.firebaseapp.com",
-  projectId: "sapiv-3fb24",
-  storageBucket: "sapiv-3fb24.appspot.com",
-  messagingSenderId: "709617536019",
-  appId: "1:709617536019:web:c6c1fbe08c7536d31fc3e6",
-  measurementId: "G-0T4X8EBXG2",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
